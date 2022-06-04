9ec13f8ba2aedfaab5f38e03cbc02fb4ee0957bc
yuya6180 <106859812+yuya6180@users.noreply.github.com>
2022-06-04 16:44:18 +0900

    initial commit
    hokusho.baseball.homepage

---
+++ pt.iphone/js/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+{
+    window.onload = function () {
+        var nav = document.getElementById('nav-wrapper');
+        var hamburger = document.getElementById('js-hamburger');
+        var blackBg = document.getElementById('js-black-bg');
+    
+        hamburger.addEventListener('click', function () {
+            nav.classList.toggle('open');
+        });
+        blackBg.addEventListener('click', function () {
+            nav.classList.remove('open');
+        });
+    };
+}
